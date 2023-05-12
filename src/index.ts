529fa32ba0686fc771297a61e8b754c59acfe7c3
Aldo Fiore <aldo.fiore95@gmail.com>
2023-05-12 11:04:53 +0200

    linting

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  console.log('hello');
   return res.send('Express + Typescript server');
 });
 
