cb558834dd5bc17f25e9d4e0fe9cdb7b2831f160
Aldo Fiore <aldo.fiore95@gmail.com>
2023-05-12 11:04:38 +0200

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
 
 dotenv.config();
 
@@ -7,10 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    console.log('hello');
-    return res.send('Express + Typescript server');
+  console.log('hello');
+  return res.send('Express + Typescript server');
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
