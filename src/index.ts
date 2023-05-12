e81b91feac7d6e5be2ece872d51c2643bbdd4483
Aldo Fiore <aldo.fiore95@gmail.com>
2023-05-12 10:59:44 +0200

    testing indentation

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
+    console.log('hello');
     return res.send('Express + Typescript server');
 });
 
